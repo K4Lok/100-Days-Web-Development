14a7e49653f2d78d166a3487fa52d3f6c99c294a
ImK4Lok <samkalok107@gmail.com>
2022-06-18 09:15:24 +0100

    init Day-42, add Project-3 content

---
+++ Code_Snippets/Project-3/scripts/config.js
@@ -0,0 +1,13 @@
+function openPlayerConfig() {
+    playerConfigOverlayElement.style.display="block";
+    backdropElement.style.display="block";
+}
+
+function closePlayerConfig() {
+    playerConfigOverlayElement.style.display="none";
+    backdropElement.style.display="none";
+}
+
+function savePlayerConfig(event) {
+    event.preventDefault();
+}
